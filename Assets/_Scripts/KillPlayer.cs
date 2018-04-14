c3a04e237aac71133ec0a1af0fcc51417224c3c7
Navin Isaiah <sprouts@endless-frontier.com>
2018-04-15 00:40:09 +0200

    Water Death

---
+++ Assets/_Scripts/KillPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillPlayer : MonoBehaviour {
+
+    public float reloadDelay = 0.2f;
+    Collider2D collidedObject;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        collidedObject = other;
+        Invoke("Reload", reloadDelay);
+    }
+
+    void Reload()
+    {
+        if (collidedObject.gameObject.CompareTag("Player")){
+            Scene loadedLevel = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(loadedLevel.buildIndex);
+        }
+    }
+}
